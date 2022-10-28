07201595eb10dcc4ca158518b2eed04f1fbf8491
Oyegoke Praise <75364938+Emerald-Realm@users.noreply.github.com>
2022-10-28 19:12:13 +0000

    fixed issues with images and slack username

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import Link from './components/Link'
-import Social from './components/Social'
+import profile from '../public/Profile.png'
+import slack from '../public/slack.png'
+import github from '../public/Social.png'
+import internship from '../public/Zuri.Internship_Logo.png'
+import ig from '../public/I4G.png'
 
 export default function App() {
 
@@ -22,7 +26,7 @@ export default function App() {
   }
   let python = {
     id: 'book_python',
-    link: 'https://books.zuri.team/python-for-beginners?ref_id=praise',
+    link: 'https://books.zuri.team/python-for-beginners?ref_id=temple-lead',
     text: 'Get Python for Beginners'
   }
   let background = {
@@ -42,7 +46,7 @@ export default function App() {
     <main>
       <div className='inner'>
         <div>
-          <img id="profile__img" src="../public/Profile%20pic%20.png" />
+          <img id="profile__img" src={profile} />
           <h3>Praise Oyegoke</h3>
           <div className='links'>
             {
@@ -52,18 +56,18 @@ export default function App() {
           </div>
           <div className='icons'>
             <a href="">
-              <img className='logo' src='../public/slack.png' />
+              <img className='logo' src={slack} />
             </a>
             <a href="https://github.com/Emerald-Realm">
-              <img className='logo' src='../public/Social%20icon.png' />
+              <img className='logo' src={github} />
             </a>
           </div>
         </div>
 
         <footer>
-          <img className='logo' src='../public/Zuri.Internship_Logo.png' />
+          <img className='logo' src={internship} />
           <p>HNG Internship 9 frontend Task</p>
-          <img className='logo' src='../public/I4G.png' />
+          <img className='logo' src={ig} />
         </footer>
       </div>
 
